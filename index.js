07e6bb386409f07f877a811397026807d20794a5
sakkuntyo <20591351+sakkuntyo@users.noreply.github.com>
2023-04-17 02:14:52 +0900

    img2img

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const fs = require("fs");
 const path = require('path');
 require('date-utils');
+const axios = require('axios');
+
 
 //bot config
 var stablediffusionDir = "D:\\stable-diffusion";
@@ -55,6 +57,37 @@ const commands = [
       .setDescription(`the model file, default ${defaultModel}, available model shows "modellist" command.`)
     ),
   new SlashCommandBuilder()
+    .setName('img2img')
+    .setDescription('generate image')
+    .addStringOption(option =>
+      option.setName('prompt')
+      .setDescription('{} not working')
+    )
+    .addStringOption(option =>
+      option.setName('negativeprompt')
+      .setDescription('default: low quality,{} not working, not working.')
+    )
+    .addStringOption(option =>
+      option.setName('imageurl')
+      .setDescription('example: https://aaa/1053631130831704145/1097154518589911081/grid-0000.png')
+    )
+    .addStringOption(option =>
+      option.setName('seed')
+      .setDescription('default 42')
+    )
+    .addStringOption(option =>
+      option.setName('numberofiterate')
+      .setDescription('default 1')
+    )
+    .addStringOption(option =>
+      option.setName('strength')
+      .setDescription('default 0.3')
+    )
+    .addStringOption(option =>
+      option.setName('ckpt')
+      .setDescription(`the model file, default ${defaultModel}, available model shows "modellist" command.`)
+    ),
+  new SlashCommandBuilder()
     .setName('modellist')
     .setDescription('show available model list')
 ].map(command => command.toJSON());
@@ -81,7 +114,6 @@ const {
   Events
 } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] ,presence: {status: "invisible"}});
-const axios = require("axios");
 
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -89,87 +121,98 @@ client.on('ready', async () => {
 
 client.on('interactionCreate', async interaction => {
   if (interaction.isChatInputCommand()){
+    var prompt = interaction.options.getString("prompt");
+    if (prompt.match(/{/g) || prompt.match(/}/g)) {
+      await interaction.followUp(`can't use these characters {,}`);
+      return;
+    }
+    prompt.replace("'","").replace("$","").replace("\"",""); //sanitize
+    var negativeprompt = interaction.options.getString("negativeprompt");
+    if (!negativeprompt) {
+      negativeprompt = "low quality";
+    }
+    negativeprompt.replace("'","").replace("$","").replace("\"",""); //sanitize
+    if (negativeprompt.match(/{/g) || negativeprompt.match(/}/g)) {
+      await interaction.followUp(`can't use these characters {,}`);
+      return;
+    }
+    var imageurl = interaction.options.getString("imageurl");
+    if (!imageurl) {
+      imageurl = "";
+    }
+    imageurl.replace("'","").replace("$","").replace("\"",""); //sanitize
+    var strength = interaction.options.getString("strength");
+    if (!strength) {
+      strength = "0.3";
+    }
+    strength.replace("'","").replace("$","").replace("\"",""); //sanitize
+    //var envlist = require('child_process').execSync("conda env list",{'windowsHide': true}).toString();
+    //if (!envlist.match(/ldm/g)){
+    //  console.log("conda not exists, or ldm env is not exists");
+    //  return;
+    //};
+    var height = interaction.options.getString("height");
+    if (!height){
+      height = "512"
+    }
+    height.replace("'","").replace("$","").replace("\"",""); //sanitize
+    height = parseInt(height) - (parseInt(height) % 64);
+    var width = interaction.options.getString("width");
+    if (!width){
+      width = "512"
+    }
+    width.replace("'","").replace("$","").replace("\"",""); //sanitize
+    width = parseInt(width) - (parseInt(width) % 64);
+    var seed = interaction.options.getString("seed");
+    if (!seed){
+      seed = "42"
+    }
+    seed.replace("'","").replace("$","").replace("\"",""); //sanitize
+    //var numberofiterate = "1"
+    var numberofiterate = interaction.options.getString("numberofiterate");
+    if (!numberofiterate){
+      numberofiterate = "1"
+    }
+    numberofiterate.replace("'","").replace("$","").replace("\"",""); //sanitize
+    var numberofsamples = "1"
+    //var numberofsamples = interaction.options.getString("numberofsamples");
+    //if (!numberofsamples){
+    //  numberofsamples = "1"
+    //}
+    //numberofsamples.replace("'","").replace("$","").replace("\"",""); //sanitize
+    var ddimsteps = interaction.options.getString("ddimsteps");
+    if (!ddimsteps){
+      ddimsteps = "30"
+    }
+    ddimsteps.replace("'","").replace("$","").replace("\"",""); //sanitize
+    var sampler = "plms"
+    //var sampler = interaction.options.getString("sampler");
+    //if (!sampler){
+    //  sampler = "plms"
+    //}
+    //sampler.replace("'","").replace("$","").replace("\"",""); //sanitize
+    var ckptfilepath = interaction.options.getString("ckpt");
+    var ckpt = interaction.options.getString("ckpt");
+    if (!ckptfilepath){
+      ckptfilepath = `${stablediffusionDir}\\${modelDir}\\${defaultModel}`;
+      ckpt = defaultModel;
+    } else {
+      ckptfilepath = `${stablediffusionDir}\\${modelDir}\\${ckpt}`;
+    }
+    ckptfilepath.replace("'","").replace("$","").replace("\"",""); //sanitize
+
+    const date = new Date();
+    const currentTime = date.toFormat('YYYYMMDDHH24MISS');
+
+    await interaction.deferReply();
+
+    const onesoutputDir = `${stablediffusionDir}\\${outputDir}\\` + currentTime
+    console.log(`prompt: ${prompt}, negativeprompt: ${negativeprompt}, height: ${height}, width: ${width}, seed: ${seed}, numberofiterate: ${numberofiterate}, numberofsamples: ${numberofsamples}, ddimsteps: ${ddimsteps}, sampler: ${sampler}, ckpt: ${ckpt}`);
+
     switch (interaction.commandName) {
       case 'txt2img':
-        await interaction.deferReply("txt2img is thinking...");
-  
-        var prompt = interaction.options.getString("prompt");
-        if (prompt.match(/{/g) || prompt.match(/}/g)) {
-          await interaction.followUp(`can't use these characters {,}`);
-          return;
-        }
-        prompt.replace("'","").replace("$","").replace("\"",""); //sanitize
-        var negativeprompt = interaction.options.getString("negativeprompt");
-        if (!negativeprompt) {
-          negativeprompt = "low quality";
-	}
-        negativeprompt.replace("'","").replace("$","").replace("\"",""); //sanitize
-        if (negativeprompt.match(/{/g) || negativeprompt.match(/}/g)) {
-          await interaction.followUp(`can't use these characters {,}`);
-          return;
-        }
-        //var envlist = require('child_process').execSync("conda env list",{'windowsHide': true}).toString();
-        //if (!envlist.match(/ldm/g)){
-        //  console.log("conda not exists, or ldm env is not exists");
-        //  return;
-        //};
-        var height = interaction.options.getString("height");
-        if (!height){
-          height = "512"
-        }
-        height.replace("'","").replace("$","").replace("\"",""); //sanitize
-        height = parseInt(height) - (parseInt(height) % 64);
-        var width = interaction.options.getString("width");
-        if (!width){
-          width = "512"
-        }
-        width.replace("'","").replace("$","").replace("\"",""); //sanitize
-        width = parseInt(width) - (parseInt(width) % 64);
-        var seed = interaction.options.getString("seed");
-        if (!seed){
-          seed = "42"
-        }
-        seed.replace("'","").replace("$","").replace("\"",""); //sanitize
-        //var numberofiterate = "1"
-        var numberofiterate = interaction.options.getString("numberofiterate");
-        if (!numberofiterate){
-          numberofiterate = "1"
-        }
-        numberofiterate.replace("'","").replace("$","").replace("\"",""); //sanitize
-        var numberofsamples = "1"
-        //var numberofsamples = interaction.options.getString("numberofsamples");
-        //if (!numberofsamples){
-        //  numberofsamples = "1"
-        //}
-        //numberofsamples.replace("'","").replace("$","").replace("\"",""); //sanitize
-        var ddimsteps = interaction.options.getString("ddimsteps");
-        if (!ddimsteps){
-          ddimsteps = "30"
-        }
-        ddimsteps.replace("'","").replace("$","").replace("\"",""); //sanitize
-        var sampler = "plms"
-        //var sampler = interaction.options.getString("sampler");
-        //if (!sampler){
-        //  sampler = "plms"
-        //}
-        //sampler.replace("'","").replace("$","").replace("\"",""); //sanitize
-        var ckptfilepath = interaction.options.getString("ckpt");
-        var ckpt = interaction.options.getString("ckpt");
-        if (!ckptfilepath){
-          ckptfilepath = `${stablediffusionDir}\\${modelDir}\\${defaultModel}`;
-          ckpt = defaultModel;
-        } else {
-          ckptfilepath = `${stablediffusionDir}\\${modelDir}\\${ckpt}`;
-	}
-        ckptfilepath.replace("'","").replace("$","").replace("\"",""); //sanitize
-
-        const date = new Date();
-        const currentTime = date.toFormat('YYYYMMDDHH24MISS');
-        var onesoutputDir = `${stablediffusionDir}\\${outputDir}\\` + currentTime
-        console.log(`prompt: ${prompt}, negativeprompt: ${negativeprompt}, height: ${height}, width: ${width}, seed: ${seed}, numberofiterate: ${numberofiterate}, numberofsamples: ${numberofsamples}, ddimsteps: ${ddimsteps}, sampler: ${sampler}, ckpt: ${ckpt}`);
         var command = `conda activate ldm;cd ${stablediffusionDir};python scripts/txt2img.py --prompt '${prompt}' --negativeprompt '${negativeprompt}' --H '${height}' --W '${width}' --seed '${seed}' --n_iter '${numberofiterate}' --n_samples '${numberofsamples}' --ddim_steps '${ddimsteps}' --plms --ckpt '${ckptfilepath}' --outdir '${onesoutputDir}'`;
         console.log("command:\n" + command)
-        //require('child_process').exec(`conda activate ldm;cd ${stablediffusionDir};python optimizedSD/optimized_txt2img.py --prompt '${prompt}'--negativeprompt '${negativeprompt}' --H '${height}' --W '${width}' --seed '${seed}' --n_iter '${numberofiterate}' --n_samples '${numberofsamples}' --ddim_steps '${ddimsteps}' --sampler '${sampler}' --ckpt '${ckptfilepath}'`, {'shell':'powershell.exe','windowsHide': true},async (err,stdout,stderr)=>{
         require('child_process').exec(command, {'shell':'powershell.exe','windowsHide': true},async (err,stdout,stderr)=>{
           if(err){
             console.error(err);
@@ -187,13 +230,44 @@ client.on('interactionCreate', async interaction => {
             outputfilepaths.push(`${onesoutputDir}\\samples\\${filename}`);
 	  });
           
-          await interaction.followUp({ content: `> /txt2img prompt: ${prompt} negativeprompt: ${negativeprompt} height: ${height} width: ${width} seed: ${seed} numberofiterate: ${numberofiterate} numberofsamples: ${numberofsamples} ddimsteps: ${ddimsteps} sampler: ${sampler}, ckpt: ${ckpt}`, files: outputfilepaths });
+          await interaction.followUp({ content: `> /txt2img prompt: ${prompt} negativeprompt: ${negativeprompt} height: ${height} width: ${width} seed: ${seed} numberofiterate: ${numberofiterate} numberofsamples: ${numberofsamples} ddimsteps: ${ddimsteps} sampler: ${sampler} ckpt: ${ckpt}`, files: outputfilepaths });
+          console.log(`send succeeded -> prompt: ${prompt}`);
+          return;
+	});
+        break;
+      case 'img2img':
+        const beforeimgpath = `${onesoutputDir}\\before.png`
+        fs.mkdirSync(`${onesoutputDir}`, { recursive: true });
+        console.log("onesoutputDir created")
+        const imgres = await axios.get(imageurl,{responseType: 'arraybuffer'});
+        fs.writeFileSync(beforeimgpath, new Buffer.from(imgres.data), 'binary');
+        console.log("before.png saved")
+
+        var command = `conda activate ldm;cd ${stablediffusionDir};python scripts/img2img.py --prompt '${prompt}' --negativeprompt '${negativeprompt}' --init-img '${beforeimgpath}' --seed '${seed}' --n_iter '${numberofiterate}' --n_samples '${numberofsamples}' --strength '${strength}' --ckpt '${ckptfilepath}' --outdir '${onesoutputDir}'`;
+        console.log("command:\n" + command)
+        require('child_process').exec(command, {'shell':'powershell.exe','windowsHide': true},async (err,stdout,stderr)=>{
+          if(err){
+            console.error(err);
+            await interaction.followUp({ content: err.toString() });
+            return;
+	  }
+          if(stderr){
+            console.error(stderr);
+	  }
+
+          console.log(stdout);
+    
+          var outputfilepaths = [];
+          fs.readdirSync(`${onesoutputDir}\\samples`).forEach((filename) => {
+            outputfilepaths.push(`${onesoutputDir}\\samples\\${filename}`);
+	  });
+          
+          await interaction.followUp({ content: `> /img2img prompt: ${prompt} negativeprompt: ${negativeprompt} imageurl: ${imageurl} seed: ${seed} numberofiterate: ${numberofiterate} strength: ${strength} ckpt: ${ckpt}`, files: outputfilepaths });
           console.log(`send succeeded -> prompt: ${prompt}`);
           return;
 	});
         break;
       case 'modellist':
-        await interaction.deferReply("txt2img is thinking...");
         const directoryPath = `${stablediffusionDir}\\${modelDir}`;
         const regexPattern = /\.ckpt$/;
 
